901be01e192ccda6d0fa2b70a3d4cdbefade4d8c
Peter <peter.warholm@gmail.com>
2024-10-27 13:01:48 +0100

    Adjusted toggleMenu function

--- main.js
+++ main.js
@@ -8,11 +8,12 @@ function main() {
 
 /** Setup Eventlisteners for Toggle-functions */
 function setupEventListerners() {
-    burgerMenu.onclick = toggleMenu;
+    navToggle.onclick = toggleMenu;
 }
 
 /** Toggle header open close */
 function toggleMenu() {
     const navToggle = document.querySelector('.nav__toggle');
-    navToggle.classList.toggle("header-open");
+    const navPrimary = document.querySelector('.nav__primary')
+    navPrimary.classList.toggle('header-open');
 };
